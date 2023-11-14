259e64e24521e1e21e5ba16223263e6f12d622f8
tim <tim@elec.ac.nz>
2023-11-14 14:02:05 +1300

    Update makefile to use a glob

--- examples/movie/download_tart_data.sh
+++ examples/movie/download_tart_data.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # pip3 install tart_tools --upgrade
 # Needs v1.1.2b1 to fix a bug when checksumming partially downloaded local files.
+TART_API=https://tart.elec.ac.nz/${TARGET}/
 for i in {1..30}
 do
-  tart_download_data --n 1 --vis
+  tart_download_data --api ${TART_API} --n 1 --vis
+  mv vis*.hdf obs_$i.hdf
   tart_calibration_data --n 1 --file obs_$i.json
   sleep 120
 done
